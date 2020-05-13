2fb4ffcbfc72019143b6b2ad62a3d6d9543dfc78
michal bandyszak <michalbandyszak@gmail.com>
2020-05-13 14:43:58 +0200

    add full js --> still not working properly

--- scripts.js
+++ scripts.js
@@ -1,24 +1,27 @@
 const calculator = {
-    displayValue: "0",
-    firstNumber: null,
-    secondNumber: null,
-    waitingForSecondNumber: false,
-    operator: null,
+  displayValue: "0",
+  firstNumber: null,
+  waitingForSecondNumber: false,
+  operator: null,
 };
 
-//W tej funkcji przyjmuję kolejną wartość z kalkulatora.
-//Jeśli nie ma kliknięto jakiegoś operatora to na wyświetlaczu pojawi się cyfra kliknięta
-//Jeśli kliknięto to sprawdzamy czy wartość na displayu jest - jak nie to wyświetlamy to co klikneliśmy, ew kolejną cyfrę
+const preformCalculation = {
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "=": (secondNumber) => secondNumber,
+};
+
 const inputDigit = (digit) => {
-  const { displayValue, waitingForSecondNumber } = calculator;
+ const { displayValue, waitingForSecondNumber } = calculator
   if (waitingForSecondNumber === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondNumber = false;
+    calculator.displayValue = digit;
+    calculator.waitingForSecondNumber = false;
   } else {
-      calculator.displayValue = displayValue === "0" ?  +digit : displayValue + digit;
-      //calculator.secondNumber = calculator.displayValue;
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
   }
-    
 };
 
 const updateDisplay = () => {
@@ -26,46 +29,25 @@ const updateDisplay = () => {
   display.value = calculator.displayValue;
 };
 
-
-calculateResult = (firstNum, secondNum) => {
-  let result = '';
-   if(calculator.operator === '+') {
-       result = +firstNum + +secondNum;
-       calculator.displayValue = result;
-   } else if(calculator.operator === '-') {
-       result = +firstNum - +secondNum;
-       calculator.displayValue = result;
-   }
-}
-
-updateDisplay();
-
 let handleOperator = (nextOperator) => {
-    const { firstNumber, secondNumber, displayValue, operator } = calculator
-    const inputValue = displayValue;
-    console.log(calculator)
-    if (firstNumber === null) {
-        calculator.firstNumber = inputValue;
-    }
+  const { firstNumber, displayValue, operator } = calculator;
+  const inputValue = parseInt(displayValue);
 
-    calculator.waitingForSecondNumber = true;
+  if (firstNumber === null) {
+    calculator.firstNumber = inputValue;
+  } else if (operator) {
+    const result = preformCalculation[operator](firstNumber, inputValue);
 
-    calculateResult(firstNumber, secondNumber, operator)
-    calculator.operator = nextOperator;
-    
-    if(nextOperator === '=') {
-        console.log(secondNumber)
-        calculateResult(firstNumber, secondNumber)
-      
-    }
-}
+    calculator.displayValue = String(result);
+    calculator.firstNumber = result;
+  }
+
+  calculator.waitingForSecondNumber = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+};
 
 const keys = document.querySelector(".calculator__keys");
-
-//Przy kliknięciu przycisku weryfikuje czy jest to liczba czy znak specjalny.
-//Jeśli to znak specjalny to odpalam funkcję handleOperator, do której daje jako argument wartość przycisku.
-//Robię update displaya
-//Lecję do funkcji inputDigit i na koneic robię ponowny update
 keys.addEventListener("click", (event) => {
   const { target } = event;
   if (!target.matches("button")) {
@@ -73,12 +55,35 @@ keys.addEventListener("click", (event) => {
   }
 
   if (target.classList.contains("operator")) {
-    handleOperator(target.value)
+    handleOperator(target.value);
     updateDisplay();
     return;
   }
 
   inputDigit(target.value);
+  console.log(calculator)
   updateDisplay();
- 
 });
+
+
+//Bugs list:
+/*
+1. Zaczynamy od znaku operatora / --> dzieją się dziwne rzeczy. first number ma przypisane 0,
+nastepnie display value ma nowy numer po kliknięciu. Klikamy znak i nam się wszystko zeruje.
+po kliknięicu = dalej jest 0/
+
+2. zaczynamy od - ==> ok, pokazuje liczby ujemne co jest spoko. Ale przy podwójnym kliknięciu
+minusa wynik sie zeruje WTF?
+
+3. Przy pierwszym mnożeniu się wysypuje.
+
+4.brak zabezpieczenia przy dzieleniu przez 0
+
+5. C przypisuje wartość 0, ale do display value a nie bezpośrednio do firstNumber
+co psuje kolejne działania.
+
+6. Po wykonaniu kolejnych ciągów operacji kalkulator się gubi.
+
+7. Przy pomyłce z wyborem znaku, kalkulator bierze pod uwage tylko pierwsze kliknięcie
+oczekując drugiej liczby.
+*/
